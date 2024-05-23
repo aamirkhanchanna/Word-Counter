3cbcfb38c27729113ec0b13c161096c8a69860e0
Aamir Khan Channa <160345903+aamirkhanchanna@users.noreply.github.com>
2024-05-23 14:27:45 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let user_ans = await inquirer.prompt({
+    type: "input",
+    name: "words",
+    message: "Please enter you sentence"
+});
+let word_count = user_ans.words.trim().split(" ").length;
+console.log(`Your sentense has ${word_count} words.`);
